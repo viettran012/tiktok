44da04e655ab777ae639fe03145d6d785af89a9a
viettran012 <vietquoc288@gmail.com>
2022-08-07 18:40:46 +0700

    Cau hinh  Router va xay dung co che tai Layout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,4 +1,4 @@
-import Header from './Header';
+import Header from '~/components/Layout/component/Header';
 import Sidebar from './Sidebar';
 
 function DefaultLayout({ children }) {
--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,4 +1,4 @@
-import { Header } from './Header';
+import Header from '~/components/Layout/component/Header';
 
 function HeaderOnly({ children }) {
     return (
